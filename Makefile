5a2024adb224fc5dd03ba41f9a829464a7184f5c
Daniel Fan <fanyuchensx@gmail.com>
2024-06-24 06:15:41 -0700

    re-use the original command for dev build
    Signed-off-by: Daniel Fan <fanyuchensx@gmail.com>

--- Makefile
+++ Makefile
@@ -26,12 +26,17 @@ BUNDLE_DEFAULT_CHANNEL := --default-channel=$(DEFAULT_CHANNEL)
 endif
 BUNDLE_METADATA_OPTS ?= $(BUNDLE_CHANNELS) $(BUNDLE_DEFAULT_CHANNEL)
 
+REGISTRY ?= quay.io/bedrockinstallerfid
+
 # IMAGE_TAG_BASE defines the docker.io namespace and part of the image name for remote images.
 # This variable is used to construct full image tags for bundle and catalog images.
 #
 # For example, running 'make bundle-build bundle-push catalog-build catalog-push' will build and push both
 # ibm.com/ibm-account-iam-operator-bundle:$VERSION and ibm.com/ibm-account-iam-operator-catalog:$VERSION.
-IMAGE_TAG_BASE ?= ibm.com/ibm-account-iam-operator
+IMAGE_TAG_BASE ?= $(REGISTRY)/ibm-account-iam-operator
+
+# IMG defines the image:tag used for the manager.
+IMG ?= $(IMAGE_TAG_BASE):v$(VERSION)
 
 # BUNDLE_IMG defines the image:tag used for the bundle.
 # You can use it as an arg. (E.g make bundle-build BUNDLE_IMG=<some-registry>/<project-name-bundle>:<tag>)
@@ -268,6 +273,10 @@ bundle-build: ## Build the bundle image.
 bundle-push: ## Push the bundle image.
 	$(MAKE) docker-push IMG=$(BUNDLE_IMG)
 
+# Build and push the bundle image.
+.PHONY: bundle-build-push
+bundle-build-push: bundle-build bundle-push
+
 .PHONY: opm
 OPM = $(LOCALBIN)/opm
 opm: ## Download opm locally if necessary.
@@ -308,3 +317,7 @@ catalog-build: opm ## Build a catalog image.
 .PHONY: catalog-push
 catalog-push: ## Push a catalog image.
 	$(MAKE) docker-push IMG=$(CATALOG_IMG)
+
+# Build and push the catalog image.
+.PHONY: catalog-build-push
+catalog-build-push: catalog-build catalog-push
--- make/ibm.mk
+++ make/ibm.mk
@@ -1,67 +1,35 @@
-REGISTRY ?= quay.io/yuchen_shen
-
-OPERATOR_IMAGE_NAME ?= ibm-account-iam-operator
-
-IMG ?= $(REGISTRY)/$(OPERATOR_IMAGE_NAME):v$(VERSION)
-
-BUNDLE_IMG ?= $(REGISTRY)/$(OPERATOR_IMAGE_NAME)-bundle:v$(VERSION)
-
-CATALOG_IMG ?= $(REGISTRY)/$(OPERATOR_IMAGE_NAME)-catalog:v$(VERSION)
-
-CONTAINER_TOOL ?= docker
-
-VERSION = 1.0.0
+DEV_VERSION ?= 0.0.1-dev
+DEV_REGISTRY ?= quay.io/yuchen_shen
 
 YQ_VERSION ?= v4.44.1
 
 # Change the image to dev when applying deployment manifests
-deploy: IMG = quay.io/bedrockinstallerfid/ibm-account-iam-operator:dev
+deploy: configure-dev
 
-# Change the image tag from VERSION to tag when building dev image
+# Configure the varaiable for the dev build
+.PHONY: configure-dev
+configure-dev:
+	$(eval REGISTRY := $(DEV_REGISTRY))
+	$(eval VERSION := $(DEV_VERSION))
+
+##@ Development Build
 .PHONY: docker-build-dev
-docker-build-dev: 
-	$(eval IMG := $(REGISTRY)/$(OPERATOR_IMAGE_NAME):dev)
-
-.PHONY: bundle-build-dev
-bundle-build-dev: 
-	$(eval BUNDLE_IMG := $(REGISTRY)/$(OPERATOR_IMAGE_NAME)-bundle:dev)
+docker-build-dev: configure-dev docker-build 
 
 .PHONY: docker-build-push-dev
-docker-build-push-dev: docker-build-dev
-	@echo "Building the $(IMG) docker dev image ..."
-	$(CONTAINER_TOOL) build -t ${IMG} .
-	$(CONTAINER_TOOL) push ${IMG}
+docker-build-push-dev: docker-build-dev docker-push
+
+.PHONY: bundle-build-dev
+bundle-build-dev: configure-dev bundle-build
 
 .PHONY: bundle-build-push-dev
-bundle-build-push-dev: bundle-build-dev
-	@echo "Building the $(BUNDLE_IMG) bundle dev image ..."
-	$(CONTAINER_TOOL) build -f bundle.Dockerfile -t $(BUNDLE_IMG) .
-	$(MAKE) docker-push IMG=$(BUNDLE_IMG)
+bundle-build-push-dev: bundle-build-dev bundle-push
+
+.PHONY: catalog-build-dev
+catalog-build-dev: configure-dev catalog-build
 
 .PHONY: catalog-build-push-dev
-catalog-build-push-dev: opm bundle-build-dev
-	$(eval CATALOG_IMG := $(REGISTRY)/$(OPERATOR_IMAGE_NAME)-catalog:dev)
-	@echo "Building the $(CATALOG_IMG) catalog dev image ..."
-	$(OPM) index add --container-tool docker --mode semver --tag $(CATALOG_IMG) --bundles $(BUNDLE_IMGS) $(FROM_INDEX_OPT)
-	$(MAKE) docker-push IMG=$(CATALOG_IMG)
-
-.PHONY: docker-build-push
-docker-build-push:
-	@echo "Building the $(IMG) docker image ..."
-	$(CONTAINER_TOOL) build -t ${IMG} .
-	$(CONTAINER_TOOL) push ${IMG}
-
-.PHONY: bundle-build-push
-bundle-build-push:
-	@echo "Building the $(BUNDLE_IMG) bundle image ..."
-	$(CONTAINER_TOOL) build -f bundle.Dockerfile -t $(BUNDLE_IMG) .
-	$(MAKE) docker-push IMG=$(BUNDLE_IMG)
-
-.PHONY: catalog-build-push
-catalog-build-push: opm
-	@echo "Building the $(CATALOG_IMG) catalog image ..."
-	$(OPM) index add --container-tool docker --mode semver --tag $(CATALOG_IMG) --bundles $(BUNDLE_IMGS) $(FROM_INDEX_OPT)
-	$(MAKE) docker-push IMG=$(CATALOG_IMG)	
+catalog-build-push-dev: catalog-build-dev catalog-push
 
 clean-before-commit:
 	cd config/manager && $(KUSTOMIZE) edit set image controller=controller:latest
@@ -69,9 +37,10 @@ clean-before-commit:
 	sed -e 's/Always/IfNotPresent/g' ./config/manager/tmp.yaml > ./config/manager/manager.yaml
 	rm ./config/manager/tmp.yaml
 
+# yq is a lightweight and portable command-line YAML processor
 .PHONY: yq
 YQ ?= $(LOCALBIN)/yq
-yq: ## Download operator-sdk locally if necessary.
+yq:
 ifeq (,$(wildcard $(YQ)))
 ifeq (, $(shell which yq 2>/dev/null))
 	@{ \
